dbf205616b66ca4e7a3442d450045fae2bce76e1
siddhit-kale <ksiddhit9@gmail.com>
2025-02-27 23:47:20 +0530

    Minimize Maximum Pair Sum in Array

---
+++ week-6/minimize maximum pair sum.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        
+        int minMaxSum = 0;
+        for (int i = 0; i < nums.size() / 2; i++) {
+            minMaxSum = max(minMaxSum, nums[i] + nums[nums.size() - 1 - i]);
+        }
+        
+        return minMaxSum;
+    }
+};

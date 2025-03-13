7078fdbf468aa2f08daa86061449ca706af23251
siddhit-kale <ksiddhit9@gmail.com>
2025-03-13 23:35:31 +0530

    find a value after the operation

---
+++ week-8/fina a value after the operation.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int result = 0;
+        
+        for (string i : operations) {  
+            if (i == "--X" || i == "X--") {
+                result--; 
+            } else {
+                result++; 
+            }
+        }
+        
+        return result;  
+    }
+};
